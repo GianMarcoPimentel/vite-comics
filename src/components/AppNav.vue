bda6f82be02cfaf71c27a2aa47d15592554db136
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-29 15:52:38 +0100

    Fine navbar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,6 +1,22 @@
 <script>
     export default{
         nome:'AppNav',
+        data(){
+            return {
+                links :[
+                    "characters",
+                    "comics",
+                    "movies",
+                    "tv",
+                    "games",
+                    "Collectibles",
+                    "videos",
+                    "fans",
+                    "news",
+                    "shop",
+                ]
+            }
+        }
     }
 </script>
 
@@ -8,20 +24,10 @@
     <nav>
         <img src="/img/dc-logo.png" alt="Logo DC">
         <ul>
-            <li>
-                <a href="">ciao</a>
+            <li v-for="currentLink in links">
+                <a href="">{{currentLink}}</a>
             </li>
-            <li>
-                <a href="">ciao</a>
-            </li><li>
-                <a href="">ciao</a>
-            </li><li>
-                <a href="">ciao</a>
-            </li><li>
-                <a href="">ciao</a>
-            </li><li>
-                <a href="">ciao</a>
-            </li>
+           
         </ul>
     </nav>
 </template>
@@ -30,17 +36,30 @@
  nav{
     height: 100px;
     display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 10px;
     img{
         height: 100%;;
     }
     ul{
         display: flex;
-        gap: 10px;
+        gap: 20px;
+        
         list-style-type: none;
         a{
             text-decoration: none;
+            color: black;
+            text-transform: uppercase;
+            &:hover{
+                color: blue;
+            }
+        }
+        li:hover{
+           
+           border-bottom: 5px blue solid;
+        }
     }
- }
 }
 
  
